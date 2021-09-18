902d9fc2c2ddc7607a0ab72d4750e075cf380ce5
primayansa09 <primayansa9wanda11@gmail.com>
2021-09-18 11:08:46 +0700

    simplenotif

---
+++ app/src/main/java/com/example/simplenotif/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.simplenotif;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int NOTIFICATION_ID = 1;
+    private static final String CHANNEL_ID = "channel_01";
+    private static final CharSequence CHANNEL_NAME = "dicoding channel";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendNotification(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/primayansa09"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.ic_bell_grey)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.ic_bell_grey))
+                .setContentTitle(getResources().getString(R.string.content_title))
+                .setContentText(getResources().getString(R.string.content_text))
+                .setSubText(getResources().getString(R.string.subtext))
+                .setAutoCancel(true);
+
+         //Untuk android Oreo ke atas perlu menambahkan notification channel
+
+        //if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+          //  NotificationChannel chanel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            //chanel.setDescription(CHANNEL_NAME.toString());
+
+            //mBuilder.setChannelId(CHANNEL_ID);
+            //if (mNotificationManager != null){
+              //  mNotificationManager.createNotificationChannel(chanel);
+           // }
+        //}
+
+        Notification notification = mBuilder.build();
+
+        if (mNotificationManager != null){
+            mNotificationManager.notify(NOTIFICATION_ID, notification);
+        }
+    }
+}
